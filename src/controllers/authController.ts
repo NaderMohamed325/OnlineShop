8579195cb5c70ff3fa0bf8df5cca09fcc8dbeca5
NaderMohamed325 <nadoorclasher@gmail.com>
2025-02-11 17:42:12 +0200

    Refactor auth controller to improve error handling and streamline response rendering

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -2,14 +2,13 @@ import {NextFunction, Request, Response} from "express";
 import {catchAsync} from "../utils/catchAsync";
 import {validationResult} from "express-validator";
 import {User} from "../models/userModel";
-import {AppError} from "../utils/AppError";
 import bcrypt from "bcrypt";
 
-const SignUpPage = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+const SignUpPage = catchAsync(async (req: Request, res: Response, _next: NextFunction) => {
     res.render('signup.ejs', {errors: null});
 });
 
-const SignUpPost = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+const SignUpPost = catchAsync(async (req: Request, res: Response, _next: NextFunction) => {
 
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -20,20 +19,20 @@ const SignUpPost = catchAsync(async (req: Request, res: Response, next: NextFunc
     res.redirect('/login');
 });
 
-const LogInPage = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+const LogInPage = catchAsync(async (req: Request, res: Response, _next: NextFunction) => {
     res.render('login.ejs', {errors: null});
 });
 
-const LoginPost = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-    const { email, password } = req.body;
-    const user = await User.findOne({ email }).select('+password');
+const LoginPost = catchAsync(async (req: Request, res: Response, _next: NextFunction) => {
+    const {email, password} = req.body;
+    const user = await User.findOne({email}).select('+password');
     if (!user) {
-        return res.render("login", { errors: [{ msg: "Invalid Email" }] });
+        return res.render("login", {errors: [{msg: "Invalid Email"}]});
     }
 
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
-        return res.render("login", { errors: [{ msg: "Invalid Password" }] });
+        return res.render("login", {errors: [{msg: "Invalid Password"}]});
     }
 
 
