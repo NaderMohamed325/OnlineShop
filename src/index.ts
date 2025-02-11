479194698178c21b10a1921e72039c8bc562e928
NaderMohamed325 <nadoorclasher@gmail.com>
2025-02-11 17:41:08 +0200

    Add user model, validation middleware, and login/signup views

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import mongoose from "mongoose";
 import { globalErrorHandler } from "./utils/errorHandler";
 import dotenv from "dotenv";
 import { productRouter } from "./routers/productRouter";
+import {authRouter} from "./routers/authRouter";
 
 dotenv.config();
 
@@ -28,11 +29,13 @@ mongoose.connect(process.env.DB_URI || `mongodb://localhost:27017/products`)
 
 app.use('/', homeRouter);
 app.use('/', productRouter);
+app.use('/',authRouter);
 app.use(globalErrorHandler);
 app.all('*', (req: Request, res: Response) => {
     res.render('error.ejs', { error: { status: 404, message: `Can't find ${req.originalUrl} on this server` } });
     console.error(`Can't find ${req.originalUrl} on this server`);
 });
+
 
 app.listen(3000, () => {
     console.log(`Server is running`);
