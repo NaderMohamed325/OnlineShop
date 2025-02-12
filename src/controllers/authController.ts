9ad082a12c185e3000dedd7d49b14a0333a8f643
NaderMohamed325 <nadoorclasher@gmail.com>
2025-02-12 14:33:51 +0200

    Implement route protection middleware and enhance session management; update navbar for user authentication

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -4,50 +4,64 @@ import {validationResult} from "express-validator";
 import {User} from "../models/userModel";
 import bcrypt from "bcrypt";
 
-const SignUpPage = catchAsync(async (req: Request, res: Response, _next: NextFunction) => {
-    res.render('signup.ejs', {errors: null});
+const SignUpPage = catchAsync(async (req: Request, res: Response) => {
+    return res.render("signup.ejs", {errors: null, isUser: req.session.userId});
 });
 
-const SignUpPost = catchAsync(async (req: Request, res: Response, _next: NextFunction) => {
+const SignUpPost = catchAsync(async (req: Request, res: Response) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        return res.render('signup.ejs', {errors: errors.array()});
+        return res.render("signup.ejs", {errors: errors.array()});
     }
+
     const {name, email, password} = req.body;
     await User.create({name, email, password});
-    res.redirect('/login');
+
+    return res.redirect("/login");
 });
 
-const LogInPage = catchAsync(async (req: Request, res: Response, _next: NextFunction) => {
-    res.render('login.ejs', {errors: null});
+const LogInPage = catchAsync(async (req: Request, res: Response) => {
+    return res.render("login.ejs", {errors: null, isUser: req.session.userId});
 });
 
-const LoginPost = catchAsync(async (req: Request, res: Response, _next: NextFunction) => {
+const LoginPost = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
     const {email, password} = req.body;
 
-    // Find the user and include password field
-    const user = await User.findOne({email}).select('+password');
+    const user = await User.findOne({email}).select("+password");
     if (!user) {
-        return res.render("login", {errors: [{msg: "Invalid credentials"}]});
+        return res.render("login.ejs", {errors: [{msg: "Invalid credentials"}]});
     }
 
-    // Compare password
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
-        return res.render("login", {errors: [{msg: "Invalid credentials"}]});
+        return res.render("login.ejs", {errors: [{msg: "Invalid credentials"}]});
     }
+    console.log("Session:", req.session);
+    console.log("Session User ID:", req.session.userId);
 
     req.session.userId = user._id as string;
-    //this is the current error i am facing
 
-    // Ensure session is saved before redirecting
     req.session.save((err) => {
         if (err) {
             console.error("Session save error:", err);
-            return res.render("login", {errors: [{msg: "Something went wrong. Please try again."}]});
+            return next(err); // Pass error to the error handler middleware
         }
-        res.redirect("/dashboard");
+        return res.redirect("/dashboard");
     });
 });
 
-export {SignUpPage, SignUpPost, LogInPage, LoginPost};
+const LogOut = catchAsync(async (req: Request, res: Response) => {
+    req.session.destroy((err) => {
+        if (err) {
+            console.error("Session destroy error:", err);
+            return res.render("error.ejs",
+                {
+                    error: {status: 500, message: "Something went wrong. Please try again."}
+                    , isUser: req.session.userId
+                });
+        }
+        return res.redirect("/login");
+    });
+});
+
+export {SignUpPage, SignUpPost, LogInPage, LoginPost, LogOut};
--- src/routers/authRouter.ts
+++ src/routers/authRouter.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import {LogInPage, LoginPost, SignUpPage, SignUpPost} from '../controllers/authController';
+import {LogInPage, LoginPost, LogOut, SignUpPage, SignUpPost} from '../controllers/authController';
 import {signInValidation} from "../utils/middleWares/validation/signupValidation";
 import {LoginValidation} from "../utils/middleWares/validation/loginValidation";
 
@@ -10,5 +10,8 @@ authRouter.post('/signup', signInValidation, SignUpPost);
 
 authRouter.get('/login', LogInPage)
     .post('/login', LoginValidation, LoginPost);
+
+authRouter.route('/logout')
+    .all(LogOut);
 
 export {authRouter};
