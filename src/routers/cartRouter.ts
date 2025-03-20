29df355567ba94df646e4776956d3805013ddd68
NaderMohamed325 <nadoorclasher@gmail.com>
2025-03-20 12:45:55 +0200

    Enhance cartRouter: add error handling for checkout route and improve code formatting

--- src/routers/cartRouter.ts
+++ src/routers/cartRouter.ts
@@ -1,21 +1,29 @@
-import express from "express";
-import {addToCart, checkout, getCart, removeFromCart} from "../controllers/cartController";
-import {cartValidation} from "../utils/middleWares/validation/cartValidation";
-import {isAuthenticated} from "../utils/middleWares/protect/routeProtection";
+import express, { Request, Response } from "express";
+import {
+  addToCart,
+  checkout,
+  getCart,
+  removeFromCart,
+} from "../controllers/cartController";
+import { cartValidation } from "../utils/middleWares/validation/cartValidation";
+import { isAuthenticated } from "../utils/middleWares/protect/routeProtection";
 
 const cartRouter = express.Router();
 
-cartRouter.route('/cart-add')
-    .post(isAuthenticated, cartValidation, addToCart);
+cartRouter.route("/cart-add").post(isAuthenticated, cartValidation, addToCart);
 
-cartRouter.route('/cart')
-    .get(isAuthenticated, getCart);
+cartRouter.route("/cart").get(isAuthenticated, getCart);
 
-cartRouter.route('/cart/:productId/delete')
-    .post(removeFromCart);
+cartRouter.route("/cart/:productId/delete").post(removeFromCart);
 
-cartRouter.route('/checkout')
-    .get(checkout);
+cartRouter
+  .route("/checkout")
+  .get(checkout)
+  .post((req: Request, res: Response) => {
+    res.render("error.ejs", {
+      error: { status: 500, message: "Under Development" },
+      isUser: req.session.userId,
+    });
+  });
 
-
-export {cartRouter};
+export { cartRouter };
